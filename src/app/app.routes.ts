f0897662a291db118a777d2d894896a2b6cfc189
Michel.M. <michel269@gmail.com>
2025-07-08 16:32:06 -0300

    Commit inicial do projeto Angular com estrutura base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [ {
+        path: '',
+        redirectTo: 'login', // Se a URL for vazia, redireciona para /login
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        // Carrega o LoginComponent apenas quando o usuário for para /login
+        loadComponent: () => import('./pages/login/login').then(c => c.Login)
+    },
+    {
+        path: 'home',
+        loadComponent: () => import('./pages/home/home').then(c => c.Home)
+    },
+    {
+        path: 'dashboard',
+        loadComponent: () => import('./pages/dashboard/dashboard').then(c => c.Dashboard)
+    }];
