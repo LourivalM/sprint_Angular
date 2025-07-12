63eebc7ef29fd344a56c100bd7d66a9cfb40b018
LourivalM <lminforcontato@gmail.com>
2025-07-12 11:42:14 -0300

    "test"

--- src/app/guards/login-guard.ts
+++ src/app/guards/login-guard.ts
@@ -1,3 +1,4 @@
+/*
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { CanActivateFn } from '@angular/router';
@@ -10,5 +11,29 @@ export const loginGuard: CanActivateFn = (route, state) => {
     router.navigate(['']);
     return false;
   }
+  return true;
+};*/
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+
+export const loginGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router);
+  const platformId = inject(PLATFORM_ID);
+
+  if (isPlatformBrowser(platformId)) {
+    const email = sessionStorage.getItem('email');
+    if (!email) {
+      alert('Você precisa estar logado para acessar esta página.');
+      router.navigate(['/']);
+      return false;
+    }
+  } else {
+    // No servidor, redirecione ou negue acesso
+    router.navigate(['/']);
+    return false;
+  }
+
   return true;
 };
